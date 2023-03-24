61a260cd2b380fd0a7c242b20e350cc07392216e
siddht1 <rt1yrru+github@gmail.com>
2023-03-24 11:31:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,20 +34,20 @@ time.innerText= `${today.getHours()}:${formatZero(today.getMinutes())}`;
 const dummyData = () => {
   mainCards.forEach((card, i) => {
     card.querySelector(".title").innerText =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+      "L k";
     card.querySelector(
       ".content"
-    ).innerText = "Aliquam vitae laoreet purus. Vivamus tincidunt nibh rhoncus, varius libero dignissim, molestie odio. Aenean sit amet felis et lectus viverra elementum. In quis tortor dignissim, ultrices odio et, dignissim quam. Donec scelerisque lacinia dolor, a pulvinar enim auctor quis. Sed mollis faucibus lacus id sagittis. Nunc et fringilla ipsum, et dignissim erat. Vivamus leo lorem, iaculis tempor quam nec, malesuada ullamcorper ipsum...".slice(
+    ).innerText = " K  ".slice(
       0,
       Math.round(Math.random() * -200)
     );
   });
 };
 
-// Weather Data for Athens from open-meteo.com
+// Weather Data from open-meteo.com
 const weatherData = async () => {
   const weather =
-    "https://api.open-meteo.com/v1/forecast?latitude=37.9792&longitude=23.7166&hourly=temperature_2m&current_weather=true";
+    "https://api.open-meteo.com/v1/forecast?latitude=28.4020&longitude=76.82602&hourly=temperature_2m&current_weather=true";
   const res = await fetch(weather);
   const data = await res.json();
 
